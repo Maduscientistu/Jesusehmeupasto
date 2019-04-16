1954047577d11212154eb154dff9dc7d338ba45d
Maduscientistu <Mariomariowin@hotmail.com>
2019-04-16 10:39:52 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ def traps(update, context):
 	context.bot.send_message(chat_id=update.message.chat_id, text="There's no greater ire,\n no greater fire\n than the one burning in my soul. \n Why make it so so vile\n Man of little faith \n Traps are all my style\n My purpose and my fate.")
 
 def sendcatpics(update, context):
-	print("vc ta bein?")
-#	fotoCat = requests.get("https://api.thecatapi.com/v1/images/search")
-#	if fotoCat.response_code == 200:
-#		fotoURL = fotoCat.json()[0]['url']
-#		print(fotoURL)
-#		context.bot.send_photo(chat_id=update.message.chat_id, photo=fotoURL)
-#	else:
-#		traps(update, context)
+	context.bot.send_message(chat_id=update.message.chat_id, text="Hello, i don't like your face. Don't talk to me, ugly people don't belong to this world. ;P")
+	fotoCat = requests.get("https://api.thecatapi.com/v1/images/search")
+	if fotoCat.response_code == 200:
+		fotoURL = fotoCat.json()[0]['url']
+		context.bot.send_message(chat_id=update.message.chat_id, text="Hello, i don't like your face. Don't talk to me, ugly people don't belong to this world. ;P")
+		context.bot.send_photo(chat_id=update.message.chat_id, photo=fotoURL)
+	else:
+		traps(update, context)
 
 dispatcher.add_handler(CommandHandler('start', start))
 dispatcher.add_handler(CommandHandler('notgay', traps))
