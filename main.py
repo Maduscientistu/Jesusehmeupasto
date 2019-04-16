310103a1b799c138b9ac049470cc9400c0ea8120
Maduscientistu <Mariomariowin@hotmail.com>
2019-04-16 09:40:01 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("I like traps!")
+import telegram
+import os
+
+TELEGRAM_KEY = os.environ.get("BOT_KEY")
+
+def start(update, context):
+	context.bot.send_message("Hello, i don't like your face. Don't talk to me, ugly people don't belong to this world. ;P")
+	
+updater = Updater(token=TELEGRAM_KEY, use_context=true)
+dispatcher = updater.dispatcher
+
+dispatcher.add_handler(CommandHandler('start', start))
+
+updater.start_polling()
