4dcf67f3a79c369486f3443cd0daa87d0b2cdcc5
Maduscientistu <Mariomariowin@hotmail.com>
2019-04-16 10:09:48 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from telegram.ext import Updater, CommandHandler
 import requests
 import os
 
+port = os.environ.get("PORT", 5000)
+
+
 TELEGRAM_KEY = os.environ.get("BOT_KEY")
 updater = Updater(token=TELEGRAM_KEY, use_context=True)
 dispatcher = updater.dispatcher
@@ -13,12 +16,12 @@ def traps(update, context):
 	context.bot.send_message(chat_id=update.message.chat_id, text="There's no greater ire,\n no greater fire\n than the one burning in my soul. \n Why make it so so vile\n Man of little faith \n Traps are all my style\n My purpose and my fate.")
 
 def sendcatpics(update, context):
-	fotoCat = requests.get("https://cataas.com/c")
-	if fotoCat.response_code == 200:
-		print(fotoCat)
-		send_photo(chat_id=update.message.chat_id, photo='https://cataas.com/c', caption='Gato')
-	else:
-		traps(update, context)
+#	fotoCat = requests.get("https://cataas.com/c")
+#	if fotoCat.response_code == 200:
+#		print(fotoCat)
+	send_photo(chat_id=update.message.chat_id, photo='https://cataas.com/c', caption='Gato')
+#	else:
+#		traps(update, context)
 
 dispatcher.add_handler(CommandHandler('start', start))
 dispatcher.add_handler(CommandHandler('notgay', traps))
