22a26d2bb3731b28e664672e50a71890824610fd
Maduscientistu <Mariomariowin@hotmail.com>
2019-04-16 09:52:04 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ from telegram.ext import Updater, CommandHandler
 import os
 
 TELEGRAM_KEY = os.environ.get("BOT_KEY")
-
-def start(update, context):
-	context.bot.send_message(chat_id=update.message.chat_id, text="Hello, i don't like your face. Don't talk to me, ugly people don't belong to this world. ;P")
-	
 updater = Updater(token=TELEGRAM_KEY, use_context=True)
 dispatcher = updater.dispatcher
 
+def start(update, context):
+	context.bot.send_message(chat_id=update.message.chat_id, text="Hello, i don't like your face. Don't talk to me, ugly people don't belong to this world. ;P")
+
+def traps(update, context):
+	context.bot.send_message(chat_id=update.message.chat_id, text="There's no greater ire,\n no greater fire\n than the one burning in my soul. \n Why make it so so vile\n Man of little faith \n Traps are all my style\n My purpose and my fate.")
+	
 dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('notgay', traps))
+
+
+
 
 updater.start_polling()
