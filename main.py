9d1c0cd814e1d8a79cae24218e407d7c47a935d3
Maduscientistu <Mariomariowin@hotmail.com>
2019-04-16 09:46:31 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 TELEGRAM_KEY = os.environ.get("BOT_KEY")
 
 def start(update, context):
-	context.bot.send_message("Hello, i don't like your face. Don't talk to me, ugly people don't belong to this world. ;P")
+	context.bot.send_message(chat_id=update.message.chat_id, text="Hello, i don't like your face. Don't talk to me, ugly people don't belong to this world. ;P")
 	
 updater = Updater(token=TELEGRAM_KEY, use_context=True)
 dispatcher = updater.dispatcher
