53127c2ddffc9a1057cf9bf8261227746952da64
Maduscientistu <Mariomariowin@hotmail.com>
2019-04-16 09:44:20 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ TELEGRAM_KEY = os.environ.get("BOT_KEY")
 def start(update, context):
 	context.bot.send_message("Hello, i don't like your face. Don't talk to me, ugly people don't belong to this world. ;P")
 	
-updater = Updater(token=TELEGRAM_KEY, use_context=true)
+updater = Updater(token=TELEGRAM_KEY, use_context=True)
 dispatcher = updater.dispatcher
 
 dispatcher.add_handler(CommandHandler('start', start))
